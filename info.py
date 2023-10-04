5227c9a3c377555dd971b2d681d5a26032386502
Rokstar890 <144723163+Rokstar890@users.noreply.github.com>
2023-10-04 22:22:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -35,15 +35,12 @@ auth_grp = environ.get('AUTH_GROUP')
 FILDLT_CNL = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('FILDLT_CNL', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://hebapir376:ravi1234@cluster0.x9pmpfx.mongodb.net/?retryWrites=true&w=majority")
 LOG_CHANNEL  = -1001940288450")
 DATABASE_NAME = environ.get('DATABASE_NAME', "EvaMaria")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Channel Button Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/iPapkornMovieGroup')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/potterhub')
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/netflixrequstgroup1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Netflixvilla_india')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '+-ojTuVVnMehiZmU1')
@@ -52,12 +49,9 @@ MSG_ALRT = environ.get('MSG_ALRT', 'Share and Support Us')
 # Custom Chats
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', 0))
 FILE_CHANNEL = int(environ.get('FILE_CHANNEL', 0))
-FILE_CHANNEL_LINK = environ.get('FILE_CHANNEL_LINK', 'https://t.me/+KxOOERk2WYdmNWM1')
 FILE_CHANNEL_LINK = environ.get('FILE_CHANNEL_LINK', 'https://t.me/+KEUXcl2WUT02MzA1')
 
 # Log Channels
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001533446425'))
-RQST_LOG_CHANNEL = int(environ.get('RQST_LOG_CHANNEL', '-1001533446425'))
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001940288450'))
 RQST_LOG_CHANNEL = int(environ.get('RQST_LOG_CHANNEL', '-1001940288450'))
 
@@ -66,14 +60,9 @@ P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 @@ -99,17 +100,17 @@ def is_enabled(value, default):
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
-
-SHORTENER_API = environ.get("SHORTENER_API", "638bea22e5b27f9420265c902cb1051100513daa")
-SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "shorturllink.in")
 SHORTENER_API = environ.get("SHORTENER_API", "00b0eacdca74780666d78f22ec43440e43994c2b")
 SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "MdiskShortner.link")
 
-SHORTENER_API2 = environ.get("SHORTENER_API2", "eO4VNo2S4vfJJwVBWuEtZnLlVzm2")
-SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "shareus.in")
 SHORTENER_API2 = environ.get("SHORTENER_API2", "00b0eacdca74780666d78f22ec43440e43994c2b")
 SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "MdiskShortner.link")
 
