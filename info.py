9427274ec2e8f2be286b1d0f104ef2f8851ca13d
Rokstar890 <144723163+Rokstar890@users.noreply.github.com>
2023-10-04 22:23:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -23,10 +23,8 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1216307744').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '1216307744').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL')
